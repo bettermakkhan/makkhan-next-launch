214bc6c081b20f0dfb18aed40ca56a14cdfea4d6
Makkhan <bettermakkhan@gmail.com>
2025-06-24 22:51:06 +0530

    add cursor pointer in btns

--- src/utils/Navigation.component.tsx
+++ src/utils/Navigation.component.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { Layout, Menu } from "antd";
 import {
   SignInButton,
-  SignUpButton,
   SignedIn,
   SignedOut,
   UserButton,
@@ -17,12 +16,12 @@ const Navigation: React.FC = () => (
       <div className="text-xl font-bold mr-8">
         <Link href="/">🧈 Makkhan</Link>
       </div>
-      <div className="flex items-center gap-4 ml-auto cursor-pointer">
+      <div className="flex items-center gap-4 ml-auto">
         <SignedOut>
           <SignInButton mode="modal" />
         </SignedOut>
         <SignedIn>
-          <UserButton /> 
+          <UserButton />
         </SignedIn>
       </div>
     </div>
